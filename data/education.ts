affa304a84cb6f67befb1daefa91659f5cc0ef7a
sayandeepdas02 <das2002sayandeep@gmail.com>
2026-04-25 11:09:27 +0530

    updated

--- data/education.ts
+++ data/education.ts
@@ -10,7 +10,7 @@ export const education: Education[] = [
     {
         degree: "Bachelor of Science in Data Science and Artificial Intelligence",
         institution: "Indian Institute of Technology (IIT) Guwahati",
-        year: "2024 - 2027",
+        year: "2023 - 2027",
         location: "Guwahati, Assam, INDIA",
         logo: "/logos/iitg.png",
     },
